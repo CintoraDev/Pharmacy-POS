a9a3e9bd991e1155539fa8f11f96be8a5ca34d9b
Carlos Cintora <carloscintora66@gmail.com>
2026-04-26 17:47:03 -0300

    feat: implement user list with database query and login required

--- pharmacy/routes/users.py
+++ pharmacy/routes/users.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
+from models import User
 
 users_bp = Blueprint('users', __name__)
 
 @users_bp.route('/users')
+@login_required
 def index():
-    return render_template('users/index.html')
+    users = User.query.all()
+    return render_template('users/index.html', users = users)
