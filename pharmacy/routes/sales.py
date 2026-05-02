bd592ee50faa62124a79c5cc15204a69d748b57b
Carlos Cintora <carloscintora66@gmail.com>
2026-05-02 17:03:09 -0300

    feat: add sales cart with real-time search, add/remove products and totals

--- pharmacy/routes/sales.py
+++ pharmacy/routes/sales.py
@@ -1,9 +1,38 @@
-from flask import Blueprint, render_template
+from flask import url_for
+from flask import redirect
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required
+from models import Product, db, Customer
 
 sales_bp = Blueprint('sales', __name__)
 
 @sales_bp.route('/sales')
 @login_required
 def index():
-    return render_template('sales/index.html')
+    customers = Customer.query.filter_by(status = True).all()
+    return render_template('sales/index.html', customers = customers)
+
+
+
+@sales_bp.route('/sales/search')
+@login_required
+def search_products():
+    q = request.args.get('q', '')
+
+    products = Product.query.filter(Product.name.ilike(f'%{q}%')).limit(10).all()
+
+    return jsonify([
+    {
+        "id": product.id,
+        "name": product.name,
+        "price": float(product.price_sale),
+        "stock": product.stock
+    }
+    
+    for product in products
+])
+
+
+
+
+    
